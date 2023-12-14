d0921fc9886d32def18b4e1825843e8fd7418746
Usama <osamasayed585@gmai.com>
2023-12-14 15:46:22 +0200

    Update the Java version

--- app/src/main/java/com/example/verifyswift/MainActivity.kt
+++ app/src/main/java/com/example/verifyswift/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.verifyswift
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.droidDevOs.verifyswift.EditOtpView
 import com.droidDevOs.verifyswift.MyOtpView
 import com.example.verifyswift.ui.theme.VerifySwiftTheme
 
@@ -13,12 +20,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyOtpView(
-                otpValue = "",
-                isError = false,
-                onOtpTextChange = {},
-                onCompletedNumbers = {},
-            )
+            var otp by remember { mutableStateOf("") }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+
+                MyOtpView(
+                    otpValue = otp,
+                    isError = false,
+                    onOtpTextChange = { otp = it },
+                    onCompletedNumbers = {},
+                )
+            }
         }
     }
 }
@@ -27,11 +41,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun EditOtpViewPreview() {
     VerifySwiftTheme {
-        MyOtpView(
-            otpValue = "",
-            isError = false,
-            onOtpTextChange = {},
-            onCompletedNumbers = {},
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            MyOtpView(
+                otpValue = "",
+                isError = false,
+                onOtpTextChange = {},
+                onCompletedNumbers = {},
+            )
+        }
     }
 }
