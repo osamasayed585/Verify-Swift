96af8539a7849bd321004595e346be0e0161869b
Usama <osamasayed585@gmai.com>
2023-12-18 18:06:58 +0200

    add shapes

--- verifySwift/src/main/java/com/droidDevOs/verifyswift/theme/Shape.kt
+++ verifySwift/src/main/java/com/droidDevOs/verifyswift/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.example.verifyswift.theme
+package com.droidDevOs.verifyswift.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
