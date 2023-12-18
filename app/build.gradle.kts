671c2ddc132fb6dd71940fd397fdca3c2d25eb00
Usama <osamasayed585@gmai.com>
2023-12-18 18:06:27 +0200

    last changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -68,6 +68,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
-    implementation("com.github.osamasayed585:Verify-Swift:1.0.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+//    implementation("com.github.osamasayed585:Verify-Swift:1.0.0")
+    implementation(project(":verifySwift"))
 }
