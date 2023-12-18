ebd8ecf0d4cc669084b9a29612852882e79b65ea
Usama <osamasayed585@gmai.com>
2023-12-18 19:46:36 +0200

    update version verify swift

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,5 +71,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
-    implementation("com.github.osamasayed585:Verify-Swift:1.0.0")
+    implementation("com.github.osamasayed585:Verify-Swift:1.1.0")
 }
