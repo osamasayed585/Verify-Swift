bac67240d556ff5fdeea52f7afa5fe0837533c24
Usama <osamasayed585@gmai.com>
2023-12-18 18:08:32 +0200

    enhancement the theme

---
+++ app/src/main/java/com/example/verifyswift/theme/Fonts.kt
@@ -0,0 +1,12 @@
+package com.example.verifyswift.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.verifyswift.R
+
+val Teshrin = FontFamily(
+    Font(R.font.teshrin_ar_lt_regular, weight = FontWeight.Normal),
+    Font(R.font.teshrin_ar_lt_bold, weight = FontWeight.Bold),
+    Font(R.font.teshrin_ar_lt_medium, weight = FontWeight.Medium)
+)
