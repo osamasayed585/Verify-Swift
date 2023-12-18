a2e09fa364ab5e36c280755726e06eecdb0d12cd
Usama <osamasayed585@gmai.com>
2023-12-18 12:37:19 +0200

    add some colors

--- app/src/main/java/com/example/verifyswift/theme/Color.kt
+++ app/src/main/java/com/example/verifyswift/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.verifyswift.ui.theme
+package com.example.verifyswift.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Dark_Background = Color(0xFF242323)
+val L_Background = Color(0xFFE7E3E3)
